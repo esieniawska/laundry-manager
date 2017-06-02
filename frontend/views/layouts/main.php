af3f8bc2f1bcd84987adf1b241348a1ecf205354
Ewa Sieniawska <ewa.m.sieniawska@gmail.com>
2017-06-02 08:46:16 +0200

    add new menuItems

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -36,13 +36,16 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+
+
+
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+
     } else {
+        $menuItems[] =  ['label' => 'Zamówienia', 'url' => ['/order']];
         $menuItems[] = '<li>'
             . Html::beginForm(['/site/logout'], 'post')
             . Html::submitButton(
