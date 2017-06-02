51c1503dba104b5bad7468340d13c1f7d06a5319
Ewa Sieniawska <ewa.m.sieniawska@gmail.com>
2017-06-02 08:30:51 +0200

    create order actions on backend

---
+++ backend/views/order/index.php
@@ -0,0 +1,50 @@
+<?php
+
+use common\models\Order;
+use yii\helpers\Html;
+use kartik\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\OrderSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Orders';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="order-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Order', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'responsive' => true,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            [
+                'attribute' => 'user_id',
+                'value' => function (Order $model) {
+                    return $model->user->username;
+                }
+            ],
+            [
+                'attribute' => 'status',
+                'format' => 'html',
+                'value' => function (Order $model) {
+                    return '<div class="label label-danger">'. $model->getStatusLabel().'</div>';
+                }
+            ],
+            'created_at',
+            'updated_at',
+            'address',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
