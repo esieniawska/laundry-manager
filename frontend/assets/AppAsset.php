49151e4e56788a3bf08a0ce500b20639b7d52819
Ewa Sieniawska <ewa.m.sieniawska@gmail.com>
2017-06-02 08:43:01 +0200

    transfer css file

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -9,15 +9,24 @@ use yii\web\AssetBundle;
  */
 class AppAsset extends AssetBundle
 {
+    public $sourcePath = '@frontend/assets';
+
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/font-awesome.css',
     ];
     public $js = [
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
+        'rmrevin\yii\fontawesome\AssetBundle',
     ];
+
+    public function init() {
+        parent::init();
+        $this->publishOptions['forceCopy'] = true;
+    }
 }
