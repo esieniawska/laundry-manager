ed8e16a57a220e5c8ad2f5065bee8c6bbc830bb8
Ewa Sieniawska <ewa.m.sieniawska@gmail.com>
2017-06-20 14:06:20 +0200

    delete order menuItem

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -38,14 +38,13 @@ AppAsset::register($this);
         ['label' => 'Home', 'url' => ['/site/index']],
 
 
-
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
 
     } else {
-        $menuItems[] =  ['label' => 'Zamówienia', 'url' => ['/order']];
+
         $menuItems[] = '<li>'
             . Html::beginForm(['/site/logout'], 'post')
             . Html::submitButton(
