d95d26fde0bb6c34f6aba88dbe9d2a86247a514d
Ewa Sieniawska <ewa.m.sieniawska@gmail.com>
2017-06-20 15:09:00 +0200

    changed directory

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -9,15 +9,25 @@ use yii\web\AssetBundle;
  */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
+
+    public $sourcePath = '@backend/assets';
+
+
     public $css = [
         'css/site.css',
     ];
+
     public $js = [
     ];
+
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
+        'rmrevin\yii\fontawesome\AssetBundle',
     ];
+
+    public function init() {
+        parent::init();
+        $this->publishOptions['forceCopy'] = true;
+    }
 }
