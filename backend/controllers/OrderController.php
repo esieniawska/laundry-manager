45325646e279a78b5ac076bf7e3266a9f369dff5
Ewa Sieniawska <ewa.m.sieniawska@gmail.com>
2017-06-21 11:26:14 +0200

    add actionWaitingForWash, actionWash, actionWaitingForReturn and removed unused actions

--- backend/controllers/OrderController.php
+++ backend/controllers/OrderController.php
@@ -2,9 +2,11 @@
 
 namespace backend\controllers;
 
+use common\models\OrderProduct;
 use Yii;
 use common\models\Order;
 use common\models\OrderSearch;
+use yii\data\ActiveDataProvider;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -51,48 +53,33 @@ class OrderController extends Controller
      */
     public function actionView($id)
     {
+        $query = OrderProduct::find()
+            ->where('order_id = :id', ['id' => $id]);
+
+        $orderProductsDataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
         return $this->render('view', [
             'model' => $this->findModel($id),
+            'orderProductsDataProvider'=>$orderProductsDataProvider
         ]);
     }
 
-    /**
-     * Creates a new Order model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new Order();
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        } else {
-            return $this->render('create', [
-                'model' => $model,
-            ]);
-        }
+    public function actionWaitingForWash($id){
+        Order::setWaitingForWashStatus($id);
+        return $this->actionIndex();
     }
 
-    /**
-     * Updates an existing Order model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        } else {
-            return $this->render('update', [
-                'model' => $model,
-            ]);
-        }
+    public function actionWash($id){
+        Order::setWashingStatus($id);
+        return $this->actionIndex();
     }
 
+    public function actionWaitingForReturn($id){
+        Order::setWaitingForReturnToCustomerStatus($id);
+        return $this->actionIndex();
+    }
     /**
      * Deletes an existing Order model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
